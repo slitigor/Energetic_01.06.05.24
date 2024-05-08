2b34f0f0607f1399b3d94cb2378e7ace6a66e11c
slitigor <slt-ig@rambler.ru>
2024-05-09 02:30:24 +0500

    Добавлен сервис SwGearService

---
+++ src/main/java/ru/slitigor/energetic/service/SwGearService.java
@@ -0,0 +1,15 @@
+package ru.slitigor.energetic.service;
+
+import ru.slitigor.energetic.model.SwGear;
+import ru.slitigor.energetic.model.enums.SGType;
+import ru.slitigor.energetic.model.enums.Voltage;
+
+import java.util.List;
+
+public interface SwGearService {
+    SwGear getByTypeVoltageSubstationId(SGType sgType, Voltage voltage, Integer id);
+    List<SwGear> getAllSwGear();
+    SwGear createSwGear(SwGear swGear);
+    SwGear updateSwGear(Long id, SwGear swGear);
+    void  deleteSwGear(Long id);
+}
---
+++ src/main/java/ru/slitigor/energetic/service/impl/SwGearServiceImpl.java
@@ -0,0 +1,71 @@
+package ru.slitigor.energetic.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.slitigor.energetic.model.Substation;
+import ru.slitigor.energetic.model.SwGear;
+import ru.slitigor.energetic.model.enums.SGType;
+import ru.slitigor.energetic.model.enums.Voltage;
+import ru.slitigor.energetic.repository.SwGearRepository;
+import ru.slitigor.energetic.service.SubstationService;
+import ru.slitigor.energetic.service.SwGearService;
+import ru.slitigor.energetic.utils.ItemAlreadyExistsException;
+import ru.slitigor.energetic.utils.ResourceNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SwGearServiceImpl implements SwGearService {
+    private final SwGearRepository repository;
+    private final SubstationService service;
+
+    @Override
+    public SwGear getByTypeVoltageSubstationId(SGType sgType, Voltage voltage, Integer id) {
+        return repository.findBySgTypeAndVoltageAndSubstation_Id(sgType, voltage, id).orElseThrow(() ->
+                new ResourceNotFoundException("SwGear", "sgType", sgType.getValue()));
+    }
+
+    @Override
+    public List<SwGear> getAllSwGear() {
+        return repository.findAll();
+    }
+
+    @Override
+    public SwGear createSwGear(SwGear swGear) {
+        Optional<SwGear> isExists = repository.findBySgTypeAndVoltageAndSubstation_Id(
+                swGear.getSgType(), swGear.getVoltage(), swGear.getSubstation().getId()
+        );
+        if (isExists.isPresent()) throw new ItemAlreadyExistsException(String.format(
+                "The switchgear with the type '%s' already exists!", swGear.getSgType().getValue()
+        ));
+        updateLinkSubstation(swGear);
+        return repository.save(swGear);
+    }
+
+    @Override
+    public SwGear updateSwGear(Long id, SwGear swGear) {
+        Optional<SwGear> isExists = repository.findById(id);
+        if (isExists.isEmpty()) throw new ResourceNotFoundException("SwGear", "id", id.toString());
+        swGear.setId(id);
+        updateLinkSubstation(swGear);
+        return repository.save(swGear);
+    }
+
+    @Override
+    public void deleteSwGear(Long id) {
+        SwGear swGear = repository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("SwGear", "id", id.toString()));
+        repository.delete(swGear);
+    }
+
+    private void updateLinkSubstation(SwGear swGear) {
+        Substation substation = service.getByNameAndDistrictId(swGear.getSubstation().getName(),
+                swGear.getSubstation().getDistrict().getId());
+        swGear.setSubstation(substation);
+        substation.getSwitchgearList().add(swGear);
+    }
+}

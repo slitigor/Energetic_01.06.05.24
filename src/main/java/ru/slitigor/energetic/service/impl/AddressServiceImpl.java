5ed39571c968fe3e3597c6ea65087c1155f7bb79
slitigor <slt-ig@rambler.ru>
2024-05-08 22:15:40 +0500

    Добавлен сервис AddressService

---
+++ src/main/java/ru/slitigor/energetic/service/impl/AddressServiceImpl.java
@@ -0,0 +1,58 @@
+package ru.slitigor.energetic.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.slitigor.energetic.model.Address;
+import ru.slitigor.energetic.repository.AddressRepository;
+import ru.slitigor.energetic.service.AddressService;
+import ru.slitigor.energetic.utils.ItemAlreadyExistsException;
+import ru.slitigor.energetic.utils.ResourceNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AddressServiceImpl implements AddressService {
+    private final AddressRepository repository;
+
+    @Override
+    public Address getAddressByZip(String zip) {
+        return repository.findByZip(zip).orElseThrow(() ->
+                new ResourceNotFoundException("Address", "zip", zip));
+    }
+
+    @Override
+    public List<Address> getAllAddress() {
+        return repository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Address createAddress(Address address) {
+        Optional<Address> isExists = repository.findByZip(address.getZip());
+        if (isExists.isPresent()) throw new ItemAlreadyExistsException(String.format(
+                "The address with the zip code '%s' already exists!", address.getZip()
+        ));
+        return repository.save(address);
+    }
+
+    @Override
+    public Address updateAddress(String zip, Address address) {
+        Optional<Address> isExists = repository.findByZip(zip);
+        if (isExists.isEmpty()) throw new ResourceNotFoundException("Address", "zip", zip);
+        address.setZip(zip);
+        address.setId(isExists.get().getId());
+        return repository.save(address);
+    }
+
+    @Override
+    @Transactional
+    public void deleteAddress(String zip) {
+        Optional<Address> isExists = repository.findByZip(zip);
+        if (isExists.isEmpty()) throw new ResourceNotFoundException("Address", "zip", zip);
+        repository.delete(isExists.get());
+    }
+}
